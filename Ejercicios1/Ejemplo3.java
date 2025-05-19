a5d32511d30c1ab84433f42d44298add549e6b70
mildreth <202400896f@utea.edu.pe>
2025-05-19 09:48:52 -0500

    yyyyyy

---
+++ Ejercicios1/Ejemplo3.java
@@ -0,0 +1,14 @@
+package Ejercicios1;
+
+public class Ejemplo3 {
+    public static void main(String[] args) {
+        System.out.println(" Ingles\tespañol");
+         System.out.println("computer\tordenador");
+         System.out.println(" student \talumno");
+         System.out.println(" ........\t........");
+         System.out.println(" ........\t........");
+         System.out.println(" ........\t........");
+        
+}
+}
+}
