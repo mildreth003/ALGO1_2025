3f7ecee88e54bbae0bfc977c39f24ae233d90ff0
mildreth <202400896f@utea.edu.pe>
2025-05-22 11:49:43 -0500

    hola

--- Ejercicios1/Ejemplo3.java
+++ Ejercicios1/Ejemplo3.java
@@ -5,10 +5,15 @@ public class Ejemplo3 {
         System.out.println(" Ingles\tespañol");
          System.out.println("computer\tordenador");
          System.out.println(" student \talumno");
-         System.out.println(" ........\t........");
-         System.out.println(" ........\t........");
-         System.out.println(" ........\t........");
-        
+         System.out.println(" cat  \tgato");
+         System.out.println("penguin \tpingüino");
+         System.out.println("machine \tmáquina");
+          System.out.println(" nature\tnaturaleza");
+         System.out.println("light \t\tluz");
+          System.out.println("green\t\tverde ");
+          System.out.println("book\t\tlibro ");
+          System.out.println("pyramid \tpirámide");
+
 }
 }
-}
+
