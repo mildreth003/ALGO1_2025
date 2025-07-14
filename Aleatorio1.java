14ab56d2ad9a23b7a1ea37011febd31b2bb1afbe
mildreth <202400896f@utea.edu.pe>
2025-07-14 09:34:31 -0500

    aleatorio

---
+++ Aleatorio1.java
@@ -0,0 +1,11 @@
+public class Aleatorio1 {
+    public static void main(String[] args) {
+       System.out.println("genera 110 numeros aleatorios"); 
+
+        for(int i = 1;i <= 20; i++ ){
+            System.out.println((int)(Math.random()* 11)+50);
+//numeros entre 20 y 25
+
+    }
+}
+}
--- Ejercicios1/Ejemplo5.java
+++ Ejercicios1/Ejemplo5.java
@@ -3,13 +3,13 @@ package Ejercicios1;
 public class Ejemplo5 {
     public static void main(String[] args) { 
       System.out.println("┌────────────────────────────────────────────────────────────────────                                      ");
-     System.out.println("|\033[34m ALGEBRA\033[37m  |  LENGUAJE     |  METODOLOGIA |    ALGORITMO| ALGORITMO  | ");
+     System.out.println("|\033[34m ALGEBRA\033[37m  |  \033[35m LENGUAJE \033[30m     |  METODOLOGIA |    ALGORITMO| ALGORITMO  | ");
      System.out.println("|─────────────────────────────────────────────────────────────────────| ");
      System.out.println("|\033[37m COMPUTO \033[95m    | INVESTIGACION |  METODOLOGIA |   \033[34m  ALGEBRA\033[37m   |  LENGUAJE   ");
      System.out.println("|─────────────────────────────────────────────────────────────────────| ");
      System.out.println("|\033[37m COMPUTO  \033[95m   |  INVESTIGACION|  METODOLOGIA |  \033[34m  ALGEBRA\033[37m    | LENGUAJE   | " );
      System.out.println("|─────────────────────────────────────────────────────────────────────|");
-     System.out.println("| \033[35mLENGUAJE \033[30m    |  ALGORITMO    |INVESTIGACION|   \033[37m  COMPUTO\033[95m     |  TUTORIA   | ");
+     System.out.println("| \033[35m LENGUAJE \033[30m    |  ALGORITMO    |INVESTIGACION|   \033[37m  COMPUTO\033[95m     |  TUTORIA   | ");
      System.out.println("|─────────────────────────────────────────────────────────────────────|");
      System.out.println("| LENGUAJE  |  ALGORITMO    |INVESTIGACION| \033[37m  COMPUTO \033[95m    |  TUTORIA   |");
       System.out.println("─────────────────────────────────────────────────────────────────────");
